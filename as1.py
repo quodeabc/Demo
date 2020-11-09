3e0c9b725333042aaca187c7eb751603e22d67d6
quodeabc <74171945+quodeabc@users.noreply.github.com>
2020-11-10 00:55:51 +0530

    Update as1.py

--- as1.py
+++ as1.py
@@ -1,7 +1,84 @@
-asdknasdkndas
+#__________________________________________________________________________________________________________________________________
+"""Coding Activity 1: Taking Input from Keyboard and Printing
 
-adsmnsadnksad
+Print 'Enter a Number'
+Take an input value from the keyboard and store it in container number
+Print 'The number you entered is' and value in the container number
 
-asdnkadskdsa
+Code Below """
 
-asdknasdksad
+
+
+
+#__________________________________________________________________________________________________________________________________
+"""Sample Activity 1: Integer Input from keyboard
+
+Take integer input from the keyboard and Store it in a variable num
+and run the code.
+
+Code Below  """
+#num is the container
+#We are taking input from keyboard and storing it in num in an integer
+num = int(input())
+
+#__________________________________________________________________________________________________________________________________
+"""Coding Activity 2: Integer Input from keyboard**
+
+Print 'Enter a Number'
+Take integer input from the keyboard and Store it in a variable num
+Print 'The number you entered is' and value in the container num
+
+Code Below """
+
+
+
+
+#__________________________________________________________________________________________________________________________________
+"""Coding Activity 3: Integer Input from keyboard
+
+Print 'Enter a Number'
+Take integer input from the keyboard and Store it in a variable numbr
+Print 'The number you entered is' and value in the container numbr
+Print 'Enter another Number'
+Take integer input from the keyboard and Store it in a variable numbr
+Print 'The second number you entered is' and value in the container numbr
+
+Code Below """
+
+
+
+
+#__________________________________________________________________________________________________________________________________
+"""Coding Activity 4: Integer Input from keyboard
+
+Print 'Enter a Number'
+Take integer input from the keyboard and Store it in a variable numbr
+Print 'Enter another Number'
+Take integer input from the keyboard and Store it in a variable number
+Print 'The second number you entered is' and value in the container number
+
+Code Below """
+
+
+
+
+
+
+#__________________________________________________________________________________________________________________________________
+"""Challenge Activity 1 : Integer Input from keyboard and Adding
+
+Print 'Enter the first Number'
+Take integer input from the keyboard and Store it in a variable num1
+Take integer input from the keyboard and Store it in a variable num2
+Add the values in *num1* and *num2* and store it in a container num3
+Print 'The Sum of the numbers you have entered is' and value in the container num3
+Hint: + is the sign used to add to values in two containers.
+
+Code Below """
+
+
+
+
+
+
+#__________________________________________________________________________________________________________________________________
