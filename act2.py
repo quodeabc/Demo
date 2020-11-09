7ceb4d570be71f7d048915aadb56961ff8b10ebc
quodeabc <74171945+quodeabc@users.noreply.github.com>
2020-11-10 01:23:33 +0530

    Create act2.py

---
+++ act2.py
@@ -0,0 +1,7 @@
+"""Coding Activity 2: Integer Input from keyboard**
+
+Print 'Enter a Number'
+Take integer input from the keyboard and Store it in a variable num
+Print 'The number you entered is' and value in the container num
+
+Code Below """
